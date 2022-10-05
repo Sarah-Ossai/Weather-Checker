03398a26ba84e5650c63079558328537cb85254c
Sarah Ossai <sarahossai6@gmail.com>
2022-10-05 05:22:02 +0100

    Complete

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
             </div>
             <div className="weather-box">
               <div className="temp">
-                {Math.round(weather.main.temp)}c
+                {Math.round(weather.main.temp)}°c
               </div>
               <div className="weather">{weather.weather[0].main}</div>
             </div>
